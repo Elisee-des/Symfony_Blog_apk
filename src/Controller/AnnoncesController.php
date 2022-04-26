fe8fb763cd24121be2e3bc401b97721b21ce5b47
Elisee-des <esabidani@gmail.com>
2022-04-26 12:04:34 +0000

    Affichage des Comments d'une annonce

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonces;
+use App\Entity\Comments;
+use App\Form\CommentsType;
 use App\Repository\AnnoncesRepository;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,15 +22,39 @@ class AnnoncesController extends AbstractController
     /**
      * @Route("/details/{slug}", name="details")
      */
-    public function details($slug, AnnoncesRepository $annoncesRepo): Response
+    public function details(Annonces $annonce, $slug, AnnoncesRepository $annoncesRepo, Request $request, ManagerRegistry $managerRegi): Response
     {
         $annonce = $annoncesRepo->findOneBy(["slug" => $slug]);
-
-        if (!$annonce) {
-            throw new NotFoundHttpException('Pas d\'annonce trouver');
+        // if (!$annonce) {
+        //     throw new NotFoundHttpException('Pas d\'annonce trouver');
             
+        // }
+
+        $comment = new Comments();
+
+        $commentForm = $this->createForm(CommentsType::class, $comment);
+
+        $commentForm->handleRequest($request);
+        
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) { 
+
+            $comment->setCreatedAt(new DateTime());
+            $comment->setAnnonces($annonce);
+
+            $em = $managerRegi->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash(
+               'success',
+               'Commentaire ajouter avec success'
+            );
+            return $this->redirectToRoute('annonces_details', ["slug" => $annonce->getSlug()]);
         }
         
-        return $this->render('annonces/details.html.twig', compact('annonce'));
+        return $this->render('annonces/details.html.twig', [
+            "annonce" => $annonce,
+            "commentForm" => $commentForm->createView()
+        ]);
     }
 }
